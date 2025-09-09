24f94246350e12af399529e20b04022d5b380d7e
Sohaibislam45 <sohaibislam45@gmail.com>
2025-09-09 21:38:23 +0600

    added card section and js for dymanic word changes

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,49 @@ const loadLesson = () => {
         })
 }
 
-const loadLevelWord=(id)=>{
-    const url=`https://openapi.programming-hero.com/api/level/${id}`
-    console.log(url)
-}
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            // hide only the lesson message part
+            const messageDiv = document.getElementById('lesson-message');
+            if (messageDiv) {
+                messageDiv.style.display = 'none';
+            }
 
+            displayLevelWord(json.data);
+        });
+};
+
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = '';
+
+    for (let word of words) {
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <!-- card design -->
+        <div class="card max-w-[545px] min-h-[370px] shadow-md">
+            <div class="text-center pt-14">
+                <h1 class="text-4xl font-semibold mb-5">${word.word}</h1>
+                <p class="text-lg font-normal mb-7">Meaning / Pronunciation</p>
+                <h1 class="text-4xl font-semibold bangla-font">${word.meaning}/ ${word.pronunciation}</h1>
+            </div>
+            <div class="flex justify-between items-center px-14 pt-9">
+                <button class="btn w-[60px] h-[60px] bg-[#1a91ff1a] hover:bg-[#1a91ff70]">
+                    <i class="fa-solid fa-circle-info text-[30px]"></i>
+                </button>
+                <button class="btn w-[60px] h-[60px] bg-[#1a91ff1a] hover:bg-[#1a91ff70]">
+                    <i class="fa-solid fa-volume-low text-[30px]"></i>
+                </button>
+            </div>
+        </div>
+        `;
+        wordContainer.appendChild(card);
+    }
+}
 
 
 const displayLessons = (lessons) => {
